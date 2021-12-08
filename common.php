a801ebb77e435acd4dc715ff3a3e6f8d18c4a9a3
rajatyadav8540 <51041014+rajatyadav8540@users.noreply.github.com>
2021-12-08 12:17:28 +0530

    first commit

---
+++ common.php
@@ -0,0 +1,2 @@
+<?php
+$con= mysqli_connect("localhost", "root","", "dpc")or die(mysqli_error($con));
---
+++ footer.php
@@ -0,0 +1,111 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <title>DPC</title>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+  </head>
+  <body>
+  <footer class="csgve-footer">
+	    <!-- Footer Widgets -->
+<div class="container">
+	<div class="row">
+		<div class="col-md-3 col-sm-6"><div class=" csgve-widget widget_media_image footer-item"><img width="114" height="114" src="" class="image wp-image-3089  attachment-full size-full" alt="" style="max-width: 100%; height: auto;" srcset="https://www.cdi.org.in/wp-content/uploads/2018/09/apple-icon-114x114.png 114w, https://www.cdi.org.in/wp-content/uploads/2018/09/apple-icon-114x114-100x100.png 100w" sizes="(max-width: 114px) 100vw, 114px" /><div class="clear"></div></div> <!-- end widget --><div class=" csgve-widget vt-text-widget footer-item"><h4 class="column-title">About Chellaram Hospital- Diabetes Care &#038; Multispeciality</h4>Chellaram Hospital- Diabetes Care & Multispeciality, a unit of Chellaram Foundation, is a Not-for-Profit Institute based in Pune.
+<div class="csgve-btn-group  csgve-main-btn-61a81edfdf1e5">
+              <a href="Overview.html"  class="csgve-btn">Read More</a>
+            </div>
+<div class="clear"></div></div> <!-- end widget --></div><div class="col-md-3 col-sm-6"><div class=" csgve-widget vt-text-widget footer-item"><h4 class="column-title">Quick Links</h4><ul class="csgve-useful-links  full-width"><li><a href="/overview/">About Us</a></li>
+
+<li><a href="/our-doctors/">Our Doctor</a></li>
+
+<li><a href="/aim-objectives/">Research</a></li>
+
+<li><a href="/courses-for-doctors/">Academics</a></li>
+
+<li><a href="/news-events/">News & Events</a></li>
+
+<li><a href="/contact-us/">Contact Us</a></li>
+
+<li><a href="/careers/">Careers</a></li>
+
+
+<li><a href="#"> </a></li>
+
+
+<li><a href="/privacy-policy/">Privacy Policy</a></li>
+
+<li><a href="/terms-and-conditions/">Terms & Conditions</a></li>
+
+<li><a href="https://www.cdi.org.in/cancellation-refund-policy/">Refund Policy</a></li>
+
+</ul>
+
+
+<div class="clear"></div></div> <!-- end widget --></div><div class="col-md-3 col-sm-6"><div class=" csgve-widget widget_media_image footer-item"><h4 class="column-title">Diabetes Health Magazine</h4><a href="http://www.diabeteshealth.co.in" target="_blank"><img width="229" height="300" src="https://www.cdi.org.in/wp-content/uploads/2020/12/dec20-jan21-229x300.jpg" class="image wp-image-7418  attachment-medium size-medium" alt="Best Multispeciality Hospital in Pune" style="max-width: 100%; height: auto;" title="Diabetes Health Magazine" /></a><div class="clear"></div></div> <!-- end widget --><div class=" csgve-widget vt-text-widget footer-item"><a href="http://www.diabeteshealth.co.in/" target="_blank" class=" csgve-simple-link-61a81edfdf894">View Details</a>
+
+<div class="clear"></div></div> <!-- end widget --><div class=" csgve-widget vt-text-widget footer-item"><a href="https://www.magzter.com/IN/Chellaram-Foundation/Diabetes-Health/Lifestyle/" target="_blank" class=" csgve-simple-link-61a81edfdf8fe">Diabetes Health Digital Magazine</a><div class="clear"></div></div> <!-- end widget --></div><div class="col-md-3 col-sm-6"><div class=" csgve-widget vt-text-widget footer-item"><h4 class="column-title">Contact Details</h4><div class="bullet-list contact-details-wrap  csgve-list-61a81edfdfa46">
+              <p>1st Floor, Lalani Quantum, Bavdhan Budruk, Pune-411021</p>
+              <ul><li><i class="fa fa-phone" aria-hidden="true"></i> Tell <a href="tel:020 66839 777 / 877" >020 66839 777 / 877</a></li><li><i class="fa fa-envelope-o" aria-hidden="true"></i> Email <a href="mailto:mail@cdi.org.in" >mail@cdi.org.in</a></li><li><i class="fa fa-globe" aria-hidden="true"></i> Website <a href="http://cdi.org.in/" target="_blank">cdi.org.in</a></li></ul></div>
+<div class="csgve-btn-group  csgve-main-btn-61a81edfdfb01">
+              <a href="https://goo.gl/maps/YvdtAT317L82"  class="csgve-btn">Get Map Direction<i class="fa fa-long-arrow-right"></i></a>
+            </div><div class="clear"></div></div> <!-- end widget --></div>	</div>
+</div>
+<!-- Footer Widgets -->		</footer>
+		<!-- Footer -->
+
+<!-- Copyright Bar -->
+<div class="csgve-copyright">
+	<div class="container">
+		<div class="col-sm-12 aligncenter">
+			© All Rights Reserved. Chellaram Hospital- Diabetes Care &amp; Multispeciality 2018.
+<div class="csgve-social  csgve-socials-61a81edfdfd1c"><a href="https://www.facebook.com/ChellaramHospital?ref=hl" target="_blank" class="icon-fa-facebook-square"><i class="fa fa-facebook-square"></i></a><a href="https://www.youtube.com/channel/UCSZMucsfdUG-uaA3DLWpuaQ" target="_blank" class="icon-fa-youtube-play"><i class="fa fa-youtube-play"></i></a><a href="https://twitter.com/Chellaram_Hosp" target="_blank" class="icon-fa-twitter"><i class="fa fa-twitter"></i></a></div>
+		</div>
+			</div>
+</div>
+<!-- Copyright Bar -->
+<!-- Csgve Back Top -->
+<div class="csgve-back-top">
+	<a href="#0"><i class="fa fa-angle-up" aria-hidden="true"></i></a>
+</div>
+<div class="container-fluid">
+</div>
+
+<link rel="stylesheet" href="/wp-content/themes/dentist-wp/bootstrap.css">	
+<script src="http://ajax.aspnetcdn.com/ajax/jquery.validate/1.11.1/jquery.validate.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+
+<div class="callto hidden-xs hidden-sm">
+<div class="calltoaction">
+<div class="col-sm-12 col-md-6 col-lg-6 middlebtn">
+<!--<button type="button" class="btn btncallto btn-lg" data-toggle="modal" data-target="#myModal">Book An Appointment</button>-->
+<a class="btn btncallto btn-lg" href="/online-consultation/"><button type="button" class="btn btncallto btn-lg">Book Appointment</button></a></div>
+<!--<div class="col-sm-12 col-md-4 col-lg-4 ">
+<button type="button" class="btn btncallto btn-lg" data-toggle="modal" data-target="#myModal1">Ask Us a Question</button>
+</div>-->
+
+
+	<div class="col-sm-12 col-md-6 col-lg-6">
+<button type="button" class="btn btncallto btn-lg">Call  020 66839 777</button>
+</div>
+	
+</div>
+</div>
+<div class="callto hidden-lg hidden-md">
+<div class="calltoaction">
+<div class="col-sm-12"style="border:1px solid #ffffff;">
+<a href="tel:020 66839 777"><button type="button" class="btn btncallto" >Call Us On 020 66839 777</button></a></div>
+<div class="col-sm-12"style="border:1px solid #ffffff;">
+<a class="btn btncallto btn-lg" href="/online-consultation/"><button type="button" class="btn btncallto btn-lg">Book Appointment</button></a>
+</div>
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+  </body>
+</html>
