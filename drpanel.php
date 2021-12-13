3a069b0acd5388d144b651df72ac8edd35545c69
Shivam Singh <51095912+codewithshiva@users.noreply.github.com>
2021-12-13 15:34:29 +0530

    changes-dr

--- drpanel.php
+++ drpanel.php
@@ -51,8 +51,21 @@
 
     <!-- Tab Content  -->
     <div class="tab-content">
-      <div id="profile" class="active tab-pane fade show p-4 p-md-5 pt-5">
+      <div id="home" class="active tab-pane fade show p-4 p-md-5 pt-5">
         <h2 class="mb-4">Dr. Saahab</h2>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
+          dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+          commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat
+          nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim
+          id est laborum.</p>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
+          dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+          commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat
+          nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim
+          id est laborum.</p>
+      </div>
+      <div id="profile" class="tab-pane fade p-4 p-md-5 pt-5">
+        <h2 class="mb-4">Profile</h2>
         <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
           dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
           commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat
