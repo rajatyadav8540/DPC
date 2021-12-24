97f0183b5375890dac4ec75c532f478e0f4f5507
Yagyik0926 <chinchuyagyik@gmail.com>
2021-12-25 00:01:46 +0530

    yagy

--- About.php
+++ About.php
@@ -1,7 +1,11 @@
-
+<style>
+    .jumbotron{
+        background-color: #00bcf5;
+    }
+</style>
 <?php include 'header.php' ?>
     <div class="my-5" style="height: 180px;"></div>
-    <div class="jumbotron bg-primary"><h1 class="text-center">About Us</h1></div>
+    <div class="jumbotron"><h1 class="text-center white">About Us</h1></div>
     <div class="container">
         <p class="" style="text-align: justify;">DPC has been providing health services to people in need for the past 14 years. We provide a wide range of state-of-the-art medicines for diagnostic and therapeutic purposes, with care and compassion. We believe that medication and personal care are a good combination to help people recover faster and better. We also have extensive knowledge including pulmonology, neurology, gynecology
         DPC has been providing health services to people in need for the past 14 years. We provide a wide range of state-of-the-art medicines for diagnostic and therapeutic purposes, with care and compassion. We believe that medication and personal care are a good combination to help people recover faster and better. We also have extensive knowledge including pulmonology, neurology, gynecology etc.
--- footer.php
+++ footer.php
@@ -1,6 +1,6 @@
 
       <footer id="footer" class="footer-area wow fadeIn">
-    
+         <div class="container">
             <div class="row">
                <div class="col-md-3">
                   <div class="logo padding">
@@ -30,7 +30,7 @@
                <div class="col-md-3">
                   <div class="subcriber-info">
                      <h3>Directions</h3>
-                     <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3485.113389974439!2d78.27095241431627!3d29.13184576805881!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x390b972107a88479%3A0xe103047cc77b583!2sTechtiutde%20Tribe%20LLP!5e0!3m2!1sen!2sin!4v1640276031887!5m2!1sen!2sin" width="250" height="250" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
+                     <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3485.113389974439!2d78.27095241431627!3d29.13184576805881!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x390b972107a88479%3A0xe103047cc77b583!2sTechtiutde%20Tribe%20LLP!5e0!3m2!1sen!2sin!4v1640276031887!5m2!1sen!2sin" width="300" height="250" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
                   </div>
                </div>
             </div>
@@ -41,7 +41,7 @@
             <div class="row">
                <div class="col-md-8">
                   <div class="footer-text">
-                     <p>© 2018 Lifecare. All Rights Reserved.</p>
+                     <p>© 2021 TechtitudeTribe LLP. All Rights Reserved.</p>
                   </div>
                </div>
                <div class="col-md-4">
@@ -67,10 +67,6 @@
       <script src="js/custom.js"></script>
       <!-- map -->
      <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCNUPWkb4Cjd7Wxo-T4uoUldFjoiUA1fJc&callback=myMap"></script>
-       <!-- Optional JavaScript -->
-   <!-- jQuery first, then Popper.js, then Bootstrap JS -->
-   <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-   <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
-   <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+ 
      </body>
 </html>
